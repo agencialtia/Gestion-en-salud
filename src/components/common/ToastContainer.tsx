754427253b708067b9837b1237735f4af152575c
agencialtia <agencialt.ia@gmail.com>
2026-08-26 17:54:54 -0400

    Delete src directory

--- src/components/common/ToastContainer.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useApp } from '../../context/AppContext';
-import { CheckCircle2, AlertCircle, AlertTriangle, Info, X } from 'lucide-react';
-
-export const ToastContainer: React.FC = () => {
-  return null;
-};
